e14d0a9c327ff7d85b964df0366ee293325381b1
Jeffery Fernandez <jeffery@fernandez.net.au>
2013-03-31 21:48:26 +1100

    Added copyright into header

--- install-jdk.sh
+++ install-jdk.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 : '
 Project: https://github.com/jeffery/oracle-java
-Author: Jeffery Fernandez
 Date: 31-03-2013
-Copyright: 2013
+
+Copyright (C) 2013  Jeffery Fernandez <jeffery@fernandez.net.au>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
 '
 
 set -e
